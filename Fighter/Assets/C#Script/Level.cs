dff52bc4e4e95bdec1b298b6b94f791c6f10f06f
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-05-25 16:19:07 +0800

    0525-16:17

---
+++ Fighter/Assets/C#Script/Level.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    [Header("下一個關卡的名稱")]
+    public string NextSceneName;
+    public int LevelID;
+
+    public void NextScene()
+    {
+        if (NextSceneName == "Menu")
+        {
+            GameObject.Find("BGM").GetComponent<AudioSource>().enabled = false;
+        }
+        Application.LoadLevel(NextSceneName);
+    }
+}
---
+++ Fighter/Assets/C#Script/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    bool ControlSound = false;
+    [Header("聲音的按鈕")]
+    public Image SoundImage;
+    [Header("聲音開啟圖片")]
+    public Sprite SoundOpenSprite;
+    [Header("聲音關閉圖片")]
+    public Sprite SoundCloseSprite;
+    [Header("音樂拉霸")]
+    public Slider SoundSlider;
+    [Header("音效拉霸")]
+    public Slider SoundEffectSlider;
+    string SaveAudioSlider = "SaveAudioSlider";
+
+    private void Start()
+    {
+        SoundControl();
+        PlayerPrefs.GetFloat(SaveAudioSlider, SoundSlider.value);
+    }
+
+    private void Update()
+    {
+        AudioListener.volume = SoundSlider.value;
+    }
+
+    public void SoundControl()
+    {
+        ControlSound = !ControlSound;
+        if (ControlSound == true)
+        {
+            AudioListener.pause = false;
+            SoundImage.sprite = SoundOpenSprite;
+        }
+        else
+        {
+            AudioListener.pause = true;
+            SoundImage.sprite = SoundCloseSprite;
+        }
+    }
+
+    public void ChangeAudioSlider()
+    {
+        if (SoundSlider.value == 0)
+        {
+            ControlSound = true;
+            SoundControl();
+        }
+        else
+        {
+            ControlSound = false;
+            SoundControl();
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void NextScene()
+    {
+        PlayerPrefs.SetFloat(SaveAudioSlider, SoundSlider.value);
+        Application.LoadLevel("Level");
+        
+    }
+}
